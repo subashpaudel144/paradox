a8cd938f8a73c642e2137ab8086678043bb639e5
Subash Paudel <subashpaudel144@gmail.com>
2022-01-17 17:10:37 +0545

    all files for princelab work

---
+++ src/Components/Last.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Styles/Last.css'
+
+function Last() {
+    return (
+        <div className='last'>
+            <div className='nature'>
+                <h1>Every inspiration from nature</h1>
+                <p>Get started using our services with a 30 day free trial</p>
+                <button>Try it now</button>
+            </div>
+        </div>
+    )
+}
+
+export default Last
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Styles/Navbar.css';
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <a href="#home">Home</a>
+            <a href="#features">Features</a>
+            <a href="#download">Downloads</a>
+            <a href="#career">Career</a>
+            <a href="#prolog">Prolog</a>
+        </div>
+    )
+}
+
+export default Navbar
